a98757fed157d7bb8991909bfaba683187b022e6
Ángel Guerrero <lasantiagoguerrero@gmail.com>
2020-10-28 07:03:18 -0600

    Agregando el footer y el navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,58 @@
 <template>
-<div class="mynavbar">nav</div>
+<div class="mynavbar">
+
+  <div class="logo dev">
+    <img src="../assets/logo.png" alt="">
+  </div>
+
+  <div class="menu">
+    <router-link v-for="item in menu" :key="item.id" :to="item.to" class="menu__item dev">{{ item.title }}</router-link> |
+  </div>
+
+</div>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    menu: [
+      { title: 'Inicio', to: '/' },
+      { title: 'Nueva imagen', to: '/new' }
+    ]
+  })
 }
 </script>
 
 <style lang="scss" scoped>
+.mynavbar {
+  border: 1px solid;
+  display: flex;
+  padding: 5px;
+  background-color: rgb(13, 21, 48);
+}
 
+.logo {
+  img {
+    height: 100%;
+    max-height: 50px;
+  }
+}
+
+.menu {
+  flex-grow: 1;
+  display: flex;
+  justify-content: flex-end;
+}
+.menu__item {
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  color: white;
+
+  &:hover {
+    color: aqua;
+  }
+}
 </style>
